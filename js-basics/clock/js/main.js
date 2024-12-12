44a0fd63b3ee7660b749689eb9a95729299c94dd
sahinoor101 <sahinoor101@gmail.com>
2024-12-12 10:11:27 +0530

    analog clock using js

--- js-basics/clock/js/main.js
+++ js-basics/clock/js/main.js
@@ -1,9 +1,11 @@
 function updateClock() {
   const now = new Date();
-  const hours = now.getHours();
+  let hours = now.getHours();
+  const meridiem=hours>=12 ? "PM" : "AM";
+  hours %=12 || 12;
   const minutes = now.getMinutes();
   const second = now.getSeconds();
-  const timeString = `${hours}:${minutes}:${second}`;
+  const timeString = `${hours}:${minutes}:${second} ${meridiem}`;
   document.getElementById("clock").textContent = timeString;
 }
 setInterval(updateClock, 1000);
